d0b056a7d852daab51ac0267337d9ee12b97fb87
AndrewWren <andrew.wren@ntlworld.com>
2021-04-17 12:49:05 +0100

    Initial commit with basic structure and utiliities

---
+++ main.py
@@ -0,0 +1,26 @@
+import scr.ml_utilities as mlu
+import scr.net_class
+
+
+def define_tuple():
+    pass
+
+
+def train_a():
+    pass
+
+
+def train_r():
+    pass
+
+
+def test_ar():
+    pass
+
+
+def understand():
+    pass
+
+
+if __name__ == '__main__':
+    pass
