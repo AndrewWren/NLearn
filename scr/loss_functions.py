89495f400a1745bee9a2792a67f354e2da115947
AndrewWren <andrew.wren@ntlworld.com>
2021-05-03 15:19:51 +0100

    corrected MSEBitsLoss and imported it into nets

---
+++ scr/loss_functions.py
@@ -0,0 +1,16 @@
+import torch
+
+
+class MSEBitsLoss(torch.nn.MSELoss):
+    def __init__(self, mu=1.):
+        super().__init__()
+        self.loss_fn = lambda x, y: torch.mean(
+            torch.sum(
+                torch.nn.functional.mse_loss(x, y, reduction='none')
+                + (mu / 2) * torch.square(x - torch.sign(x)),
+                dim=1
+            )
+        )
+
+    def forward(self, input, target):
+        return self.loss_fn(input, target)
