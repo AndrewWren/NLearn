fbc3833ba1e3f7134eaf996be7a0bc5a836f6f6e
AndrewWren <andrew.wren@ntlworld.com>
2021-05-08 15:50:07 +0100

    script to generate random seeds for use in config

--- src/lib/random_seed_generator.py
+++ src/lib/random_seed_generator.py
@@ -9,6 +9,9 @@ HIGH = 10 ** 6
 rng = np.random.default_rng()
 n = int(input('How many sets of four seeds?'))
 seeds = rng.integers(HIGH, size=(n, 4))
+print('[')
 for i, seed in enumerate(seeds):
-    print(f'\n{tuple(seed)},', end='')
-print('\b')
+    if i > 0:
+        print('')
+    print(f'{tuple(seed)},', end='')
+print('\b\n],')
