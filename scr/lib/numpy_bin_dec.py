a3664a0747917d408993a07612630bc611426eca
AndrewWren <andrew.wren@ntlworld.com>
2021-05-08 10:45:14 +0100

    bin dec conversion utilities for numpy and torch

---
+++ scr/lib/numpy_bin_dec.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+BINARY_LENGTH = 8   # Change what BINARY_LENGTH is as needed
+
+BIN_TEMPLATE = np.array([2 ** n for n in range(BINARY_LENGTH - 1, -1, -1)])
+DEC_TEMPLATE = BIN_TEMPLATE / 2
+
+
+def dec_2_bin(d_array):
+    return 2 * (d_array & BIN_TEMPLATE) / BIN_TEMPLATE - 1
+
+
+def bin_2_dec(b_array):
+    return np.sum((b_array + 1) * DEC_TEMPLATE)
---
+++ scr/lib/torch_bin_dec.py
@@ -0,0 +1,19 @@
+import torch
+from src.ml_utilities import c
+
+
+BINARY_LENGTH = c.N_CODE  # Change what BINARY_LENGTH is as needed
+
+BIN_TEMPLATE = torch.LongTensor([2 ** n for n in range(BINARY_LENGTH - 1, -1,
+                                                        -1)])
+DEC_TEMPLATE = BIN_TEMPLATE / 2
+
+
+def dec_2_bin(d_tensor):
+    d_tensor = d_tensor.unsqueeze(-1).repeat(1, BINARY_LENGTH)
+    return 2 * torch.bitwise_and(BIN_TEMPLATE, d_tensor) / BIN_TEMPLATE \
+           - 1
+
+
+def bin_2_dec(b_tensor):
+    return torch.sum(((b_tensor + 1) * DEC_TEMPLATE), dim=-1)
